66ca2102bb5494395f0835a6463d63a9d1962b74
DrCreo <DrCreo1@gmail.com>
2018-09-02 18:50:16 +0100

    More stuff also Hi death :eyes:

---
+++ Assets/Code/Menus and UI/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public string SceneName;
+    float timer = 0f;
+    float timerMax = .05f;
+    private bool pressedPlay = false;
+
+    public GameObject BlackScreen;
+
+
+    // Update is called once per frame
+    void Update () {
+        if (!pressedPlay)
+            return;
+        timer += Time.deltaTime;
+
+        if (timer >= timerMax)
+        {
+            var color = BlackScreen.GetComponent<Image>().color;
+            BlackScreen.GetComponent<Image>().color = new Color(color.r, color.g, color.b, color.a + .04f);
+            timer = 0;
+            Debug.Log(color.a);
+        }
+
+        if (BlackScreen.GetComponent<Image>().color.a >= 1f)
+            SceneManager.LoadScene(SceneName);
+    }
+
+    public void Play()
+    {
+        BlackScreen.SetActive(true);
+        pressedPlay = true;
+    }
+}
