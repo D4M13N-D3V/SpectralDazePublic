5238c2061352d36ad8fdfc83706a58cddfcad441
Death <DeathGameDev@gmail.com>
2018-09-08 11:18:20 -0500

    Map Stuff before I forget

---
+++ Assets/Code/Level System/LevelDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SpectralDaze.Levels
+{
+	public static class LevelDB
+	{
+		public static Level[] Levels;
+		public static Tileset[] Tilesets;
+
+		[RuntimeInitializeOnLoadMethod]
+		private static void Initialize()
+		{
+			Levels = Resources.LoadAll<Level>("");
+			Tilesets = Resources.LoadAll<Tileset>("");
+		}
+	}
+}
