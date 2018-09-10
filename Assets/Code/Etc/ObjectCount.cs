e035c5a3938d99c00fb90131cbc56adc9843a71e
Death <DeathGameDev@gmail.com>
2018-09-10 08:04:18 -0500

    Added odin and SabreCSG, removed level editor code

---
+++ Assets/Code/Etc/ObjectCount.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCount : MonoBehaviour
+{
+	private void OnGUI()
+	{
+		GUI.Label(new Rect(0, 0, 100, 100), GameObject.FindObjectsOfType<GameObject>().Length + "");
+	}
+}
