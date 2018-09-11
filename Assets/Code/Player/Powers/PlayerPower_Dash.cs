adf6b6304d224abf5a4e531e24fb7d528ec4af89
Trinket Entertainment <damienostler1@outlook.com>
2018-09-10 23:54:54 -0400

    Changed how dashing works and fixed particles to be bi color

--- Assets/Code/Player/Powers/PlayerPower_Dash.cs
+++ Assets/Code/Player/Powers/PlayerPower_Dash.cs
@@ -17,6 +17,8 @@ namespace SpectralDaze.Player
         public GameObject ParticleSystem;
         private bool _isDashing = false;
         private ParticleSystem _particleSystem;
+        private Vector3 _originalPos;
+        private Vector3 _lastPos;
 
         public override void Init(PlayerController pc)
         {
@@ -30,38 +32,51 @@ namespace SpectralDaze.Player
             RaycastHit mouseHit;
             if (!Physics.Raycast(UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition), out mouseHit))
                 return;
-            NavMeshHit hit;
-            NavMeshPath path = new NavMeshPath(); ;
 
-            var distBetweenMouseAndPlayer = Vector3.Distance(pc.transform.position, mouseHit.point);
+            /*
+            //NavMeshHit hit;
+            //NavMeshPath path = new NavMeshPath(); ;
+
+            //var distBetweenMouseAndPlayer = Vector3.Distance(pc.transform.position, mouseHit.point);
 
             // Switch this out with something better.
-            if (mouseHit.collider.gameObject.tag != "Walkable")
-                return;
+            //if (mouseHit.collider.gameObject.tag != "Walkable")
+            //    return;
             
-            if (Vector3.Distance(mouseHit.point, pc.transform.position) > MaximumDashDistance)
-                return;
-            if (!NavMesh.SamplePosition(mouseHit.point, out hit, 1, NavMesh.AllAreas))
-                return;
+            //if (Vector3.Distance(mouseHit.point, pc.transform.position) > MaximumDashDistance)
+            //    return;
+            
+            //if (!NavMesh.SamplePosition(mouseHit.point, out hit, 1, NavMesh.AllAreas))
+            //    return;
 
-            pc.Agent.CalculatePath(hit.position, path);
-            if(path.status != NavMeshPathStatus.PathComplete)
-                return;;
+            //pc.Agent.CalculatePath(hit.position, path);
+            //if(path.status != NavMeshPathStatus.PathComplete)
+            //    return;; */
 
+            if (_isDashing)
+            {Debug.Log(pc.transform.position + pc.transform.forward * DashSpeed * UnityEngine.Time.deltaTime);
+                pc.transform.position = pc.transform.position + pc.transform.forward * DashSpeed* UnityEngine.Time.deltaTime;
+            }
 
-            if ( !_isDashing && Input.GetMouseButtonDown(0))
+            if (_isDashing && System.Math.Round(_lastPos.x, 1) == System.Math.Round(pc.transform.position.x, 1)
+                           && System.Math.Round(_lastPos.y, 1) == System.Math.Round(pc.transform.position.y, 1)
+                           && System.Math.Round(_lastPos.z, 1) == System.Math.Round(pc.transform.position.z, 1)
+                           || Vector3.Distance(pc.transform.position, _originalPos) > MaximumDashDistance)
             {
-                pc.transform.rotation = Quaternion.LookRotation(hit.position-pc.transform.position);
-                pc.transform.eulerAngles = new Vector3(0,pc.transform.eulerAngles.y,0);
-                _isDashing = true;
+                _particleSystem.Stop();
+                _isDashing = false;
+            }
+            _lastPos = pc.transform.position;
+
+            if (!_isDashing && Input.GetMouseButtonDown(0))
+            {
+                pc.transform.rotation = Quaternion.LookRotation(mouseHit.point - pc.transform.position);
+                pc.transform.eulerAngles = new Vector3(0, pc.transform.eulerAngles.y, 0);
                 _particleSystem.Play();
+                _originalPos = pc.transform.position;
                 UnityEngine.Camera.main.gameObject.GetComponent<CameraFunctions>().Shake(0.05f, 0.2f);
-                LeanTween.move(pc.gameObject, hit.position+Vector3.up, DashSpeed * distBetweenMouseAndPlayer).setOnComplete(() =>
-                {
-                    _particleSystem.Stop();
-                    _isDashing = false;;
-                });
+                _isDashing = true;
             }
         }
     }
-}   
+}
