14a25e56d8528e932a182afef03d1d705aa82bc8
DrCreo <DrCreo1@gmail.com>
2018-09-14 16:17:01 +0100

    Added effects,sound effects and gamefeel also made a test level

---
+++ Assets/Code/Player/PlayerAnimationEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationEventHandler : MonoBehaviour
+{
+    public AudioClip FootStep;
+    public float MaxPitch = 1f;
+    public float MinPitch = .5f;
+    private AudioSource myAudioSource;
+
+    public bool FinishedEmote;
+
+    private void Start()
+    {
+        FinishedEmote = false;
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void FinishEmote()
+    {
+        FinishedEmote = true;
+    }
+
+    public void ResetEmoteState()
+    {
+        FinishedEmote = false;
+    }
+
+    public void PlayFootStepSFX()
+    {
+        myAudioSource.clip = FootStep;
+        myAudioSource.pitch = Random.Range(MinPitch, MaxPitch);
+        myAudioSource.Play();
+    }
+}
