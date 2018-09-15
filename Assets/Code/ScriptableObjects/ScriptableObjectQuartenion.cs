6811b5ca2d537e48607b207b1b1df7a7e59d08bd
Trinket Entertainment <damienostler1@outlook.com>
2018-09-15 08:29:41 -0400

    Fixed a problem with quaternion scriptable object
    Fixed a problem with quaternion scriptable object

---
+++ Assets/Code/ScriptableObjects/ScriptableObjectQuartenion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpectralDaze.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Spectral Daze/SO Variables/Quarternion")]
+    public class ScriptableObjectQuartenion : ScriptableObject
+    {
+        public Quaternion Value;
+    }
+}
