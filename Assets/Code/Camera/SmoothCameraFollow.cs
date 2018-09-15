a044a90aed92dc101b18abb35e908d587ea8a60a
Trinket Entertainment <damienostler1@outlook.com>
2018-09-15 07:08:15 -0400

    Added a thing that makes adds padding based on mouse movement
    Added a thing that makes adds padding based on mouse movement

--- Assets/Code/Camera/SmoothCameraFollow.cs
+++ Assets/Code/Camera/SmoothCameraFollow.cs
@@ -8,15 +8,32 @@ namespace SpectralDaze.Camera
     {
 
         public Transform Target;
-
-        public float Smoothness = .2f;
+        public float Smoothness = 0.2f;
+        public Vector3 MaxPadding = new Vector3(1,1,1);
+        public Vector3 MinPadding = new Vector3(-1,-1,-1);
         public Vector3 Offset = Vector3.zero;
-
+        private Vector3 InputOffset = Vector3.zero;
         void LateUpdate()
         {
             if (Target)
             {
-                var tempVec = Target.position + Offset;
+                Debug.Log(Input.GetAxis("Mouse X"));
+                Debug.Log(Input.GetAxis("Mouse Y"));
+                InputOffset += new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+
+                if (InputOffset.x > MaxPadding.x)
+                    InputOffset.x = MaxPadding.x;
+
+                if (InputOffset.z > MaxPadding.z)
+                    InputOffset.z = MaxPadding.z;
+
+                if (InputOffset.x < MinPadding.x)
+                    InputOffset.x = MinPadding.x;
+
+                if (InputOffset.z < MinPadding.z)
+                    InputOffset.z = MinPadding.z;
+
+                var tempVec = Target.position + Offset + InputOffset;
                 transform.position = Vector3.Lerp(transform.position, tempVec, Smoothness);
             }
         }
