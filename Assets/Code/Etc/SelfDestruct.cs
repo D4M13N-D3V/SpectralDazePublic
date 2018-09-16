11718f704d9ffe9cbc63446830f40f43c2ef33b3
Trinket Entertainment <damienostler1@outlook.com>
2018-09-16 00:10:48 -0400

    self destruct if miss

---
+++ Assets/Code/Etc/SelfDestruct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float SelfDestructTime = 1.5f;
+	void Start ()
+	{
+	    StartCoroutine(SelfDestructMethod());
+	}
+
+    IEnumerator SelfDestructMethod()
+    {
+        yield return new WaitForSeconds(SelfDestructTime);
+        Destroy(gameObject);
+    }
+
+}
